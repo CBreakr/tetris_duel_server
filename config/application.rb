e822cb104f7181334c9d46d4e15f32cf02212cb9
CBreakr <github email address>
2020-06-14 11:30:52 -0400

    undo last test

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,7 @@ module TetrisDuelServer
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-          origins 'https://flatirontetrisXX.herokuapp.com/'
+          origins 'https://flatirontetris.herokuapp.com/'
           resource '*', headers: :any, methods: [:get, :post, :patch, :delete]
       end
     end
